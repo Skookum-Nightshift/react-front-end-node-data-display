a00022b07fe8a2759c71584b7939d48908c39ed2
Brandon Fancher <brandonfancher@gmail.com>
2015-10-16 19:08:49 -0400

    Added more comments to the react app to aid in understanding.

--- handlers/App/index.js
+++ handlers/App/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Resolver} from 'react-resolver';
 
-/* TODOs: 
-* Guide the user through creating the budget one step at a time.
-* Set up a fail state for cases when the user is too greedy up front.
-* Make it so that setbacks are only seen once. (I'm thinking about using the visited flag on the object for that.)
-* Keep users from skipping ahead.
-* Make it pretty.
-* More comments!
-* Refactor the heck out of this thing.
-*/
 
+/**  
+ * This should be moved to its own JSON file. The app follows the order of this array. 
+ *
+ * name (string): the page's heading
+ * desc (string): The description under the icon/image.
+ * options (array of integers): each item can have as many or as few budget options as necessary.
+ * set (integer, default: null): The app currently uses this to set state for remembering what option was 
+ *   selected. Move out of appAreas and into app state? One benefit is that this can be used to set an  
+ *   optional default other than null.
+ * type (string: normal, fact, setback, etc.) When set to "normal", the item name will appear in the drawer.
+ * optionDesc: an array of strings, each coinciding with the earlier array of options.
+ */
 let appAreas = [
   {name: "Housing", desc: "Choose a housing budget.", options: [600, 700, 900], set: null, type: "normal",
    sectionImage: "http://simpleicon.com/wp-content/uploads/home-7.png",
@@ -91,11 +94,13 @@ let Options = React.createClass({
   setBalance (item, cost, choice) {
     let bal = this.props.balance;
     
-    /* If the budget for the item has not been set, set it to the cost of
-    the item clicked and deduct that from the budget. If it HAS been set
-    and your changing your choice, add the originally set amount back into 
-    the budget, update the item's 'set' key, and deduct the newly selected
-    amount from the budget. */
+    /**
+     * If the budget for the item has not been set, set it to the cost of
+     * the item clicked and deduct that from the budget. If it HAS been set
+     * and you're changing your choice, add the originally set amount back into 
+     * the budget, update the item's 'set' key, and deduct the newly selected
+     * amount from the budget. 
+     */
     if (item['set'] === null) {
       item['set'] = cost;
       bal -= cost;
@@ -123,12 +128,14 @@ let Options = React.createClass({
     return (
       item.options.map((cost, index) =>
         
-        /* When a selection is made, update the balance, set the class
-        (to make the selection red), and then determine whether the selection
-        should be disabled or not. If a selection has been made and the button's
-        cost is less than the selection, it should always be enabled. You always
-        want to allow people to decrease their budget. If no selection has been
-        made, then disable whatever doesn't fit within their budget. */
+        /** 
+         * When a selection is made, update the balance, set the class
+         * (to make the selection red), and then determine whether the selection
+         * should be disabled or not. If a selection has been made and the button's
+         * cost is less than the selection, it should always be enabled. You always
+         * want to allow people to decrease their budget. If no selection has been
+         * made, then disable whatever doesn't fit within their budget.
+         */
         <button type="button" 
                 onClick={this.setBalance.bind(this, item, cost, index)}
                 className={cost === item.set ? "activeOption" : null}
@@ -164,8 +171,15 @@ let App = React.createClass({
     };
   },
   
+
+  /**
+   * Sets the state of activeItemIndex to the index of the clicked LI, forces a re-render.
+   * This runs when a page is accessed from a click on an item in the drawer. That means 
+   * that the person has diverged from the normal order of the pages. So it sets the 'diverged'
+   * flag to true. That way, the app can decide if it should display the next item or the item
+   * that was clicked.
+   */
   handleItem (activeItemIndex) {
-    // sets the state of activeItemIndex to the index of the clicked LI forces a re-render
     this.setState({activeItemIndex});
     this.setState({diverged: true});
   },
@@ -174,7 +188,7 @@ let App = React.createClass({
   // Don't let the balance get below 0
   handleBalance (balance) {
     if (balance < 0) {
-      console.log('no way!') // TODO: Make a better message appear in the body
+      console.log('no way!')
     } else {
       this.setState({balance}); 
     }
@@ -187,11 +201,19 @@ let App = React.createClass({
     this.setState({page: page + 1, diverged: false, activeItemIndex: page});
   },
   
+  /**
+   This generates everything between the drawer and the running balance.
+   */
   cyclePage (item) {
-    console.log("Page: " + this.state.page);
-    console.log("ActiveItemIndex: " + this.state.activeItemIndex);
-    console.log("Diverged: " + this.state.diverged);
+    console.log("Page: " + this.state.page); // For debugging
+    console.log("ActiveItemIndex: " + this.state.activeItemIndex); // For debugging
+    console.log("Diverged: " + this.state.diverged); // For debugging
     
+    /**
+     * By default, the item displayed is what you click on in the drawer.
+     * But if you're clicking Next (state is not diverged from order), then
+     * override that and set the item to the next page.
+     */
     if (!this.state.diverged) {
       item = appAreas[this.state.page-1];
     };
@@ -222,8 +244,8 @@ let App = React.createClass({
       
         <div className="itemDrawer">
           <MenuItems 
-            onActivate={this.handleItem} 
-            activeItemIndex={this.state.activeItemIndex} 
+            onActivate={this.handleItem}  // give the MenuItems component access to this.handleItem via this.props.onActivate
+            activeItemIndex={this.state.activeItemIndex}  // give the MenuItems component the ability to pass things to App's state
             diverged={this.state.diverged}
             menuItems={appAreas} />
         </div>
