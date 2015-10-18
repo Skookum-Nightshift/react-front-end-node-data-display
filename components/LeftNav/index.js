0e697c6620faa79f77e48feac8f8037ac31dfe9d
babakkeyvani <babak.keyvani@cpcc.edu>
2015-10-18 16:01:08 -0400

    created working leftnav with menuitems included

---
+++ components/LeftNav/index.js
@@ -0,0 +1,32 @@
+/** @flow */
+
+require('./styles.css');
+
+import React from 'react';
+import MenuItems from '../../handlers/App/MenuItems';
+var {PropTypes} = React;
+
+class LeftNav extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render(): ?ReactElement {
+    var className = "LeftNav";
+    if (this.props.open) {
+      className += " is-open";
+    }
+    return (
+      <div className={className}>
+        <MenuItems {...this.props} />
+      </div>
+    );
+  }
+}
+
+LeftNav.propTypes = {
+  open: PropTypes.bool.isRequired,
+};
+
+export default LeftNav;
